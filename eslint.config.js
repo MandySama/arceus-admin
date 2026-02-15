fcd457e6b95ba224691712d3169751d5b9b49a79
Totodile <10122342+totodile@user.noreply.gitee.com>
2026-02-15 16:42:00 +0800

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,8 @@ import pluginVue from 'eslint-plugin-vue'
 import pluginOxlint from 'eslint-plugin-oxlint'
 import skipFormatting from 'eslint-config-prettier/flat'
 
+import pluginPrettier from 'eslint-plugin-prettier'
+
 export default defineConfig([
   {
     name: 'app/files-to-lint',
@@ -25,6 +27,29 @@ export default defineConfig([
   ...pluginVue.configs['flat/essential'],
 
   ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
+
+  {
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      // https://prettier.io/docs/options
+      'prettier/prettier': [
+        'warn',
+        {
+          printWidth: 100,
+          semi: false,
+          singleQuote: true,
+          trailingComma: 'es5',
+          endOfLine: 'auto',
+        },
+      ],
+      // https://eslint.vuejs.org/rules/
+      'vue/multi-word-component-names': 'off',
+      // https://eslint.org/docs/latest/rules/
+      'no-undef': 'error',
+    },
+  },
 
   skipFormatting,
 ])
