1cab3f329f6a9c7dfc72509237152fffed11efb9
Totodile <10122342+totodile@user.noreply.gitee.com>
2026-02-15 12:00:38 +0800

    legacy

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,22 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import legacy from '@vitejs/plugin-legacy'
+
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? './' : '/',
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-})
+}))
