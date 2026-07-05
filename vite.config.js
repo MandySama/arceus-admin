37479d36386654bdbf24d2dcfe22a6c0f6a3e600
Totodile <10122342+totodile@user.noreply.gitee.com>
2026-07-05 16:09:45 +0800

    vite

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,15 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    // vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  server: {
+    host: '0.0.0.0',
+    // open: true,
   },
 })
