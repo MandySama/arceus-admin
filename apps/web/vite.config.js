a4323826a55bb0779f3544bdc6fda480e031f3cd
Totodile <10122342+totodile@user.noreply.gitee.com>
2026-08-25 17:03:35 +0800

    monorepo

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -44,7 +44,7 @@ export default defineConfig(({ mode }) => {
       VitePWA({
         registerType: 'autoUpdate',
         devOptions: {
-          // enabled: true,
+          enabled: true,
           navigateFallbackAllowlist: [/^\/.*$/],
         },
         manifest: {
